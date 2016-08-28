922726ac74930bd0b2db0733d7d205c661c4d3a1
Hoang Mirs <hoang.mirs@gmail.com>
2016-08-29 00:56:21 +0700

    Create/Show subjects

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   root "static_pages#home"
 
+  namespace :admin do
+    root "subjects#index"
+    resources :subjects
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
