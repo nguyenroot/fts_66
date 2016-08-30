e0aaea150904a1d30ca14a109b05d4b2b0afb4a2
Hoang Mirs <hoang.mirs@gmail.com>
2016-08-30 21:58:46 +0700

    Add omniauth google facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
+
   resources :users do
     resources :questions
   end
+
   root "static_pages#home"
 
   namespace :admin do
