1a4d665d7e568e343e556c743bfca23143b38e30
Hoang Mirs <hoang.mirs@gmail.com>
2016-09-05 23:05:22 +0700

    Edit contributed questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,7 +8,6 @@ class QuestionsController < ApplicationController
 
   def new
     load_all_subject
-    @question = current_user.questions.new
     @question.answers.new
   end
 
@@ -21,6 +20,33 @@ class QuestionsController < ApplicationController
       flash[:danger] = t "flash.danger.contributed_question"
       render :new
     end
+  end
+
+  def show
+  end
+
+  def edit
+    load_all_subject
+  end
+
+  def update
+    if @question.update_attributes question_params
+      flash[:success] = t "flash.success.updated_contributed_question"
+      redirect_to user_questions_path
+    else
+      flash[:danger] = t "flash.danger.updated_contributed_question"
+      load_all_subject
+      render :edit
+    end
+  end
+
+  def destroy
+    if @question.destroy
+      flash[:success] = t "flash.success.deleted_contributed_question"
+    else
+      flash[:danger] = t "flash.danger.deleted_contributed_question"
+    end
+    redirect_to user_questions_path
   end
 
   private
