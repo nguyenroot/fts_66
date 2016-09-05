ab26900c872b37de52ba6d757744c258bb7c6713
Man Phan <phandinhman@gmail.com>
2016-09-05 09:17:19 +0700

    Edit Delete Question

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,5 +1,6 @@
 class Admin::QuestionsController < ApplicationController
   before_action :authenticate_user!, :load_questions
+  before_action :load_question, only: [:edit, :update, :destroy, :show]
   def index
   end
 
@@ -15,6 +16,18 @@ class Admin::QuestionsController < ApplicationController
       flash.now[:success] = t "flash.success.created_question"
     else
       flash.now[:danger] = t "flash.danger.created_question"
+    end
+  end
+
+  def edit
+    @subjects = Subject.all
+  end
+
+  def update
+    if @question.update_attributes question_params
+      flash.now[:success] = t "flash.success.updated_question"
+    else
+      flash.now[:danger] = t "flash.danger.updated_question"
     end
   end
 
@@ -34,6 +47,13 @@ class Admin::QuestionsController < ApplicationController
     params.require(:question).permit :id, :content, :answer_type,
       :status, :subject_id, :user_id,
       answers_attributes: [:id, :content, :is_correct, :question_id]
+  end
+
+  def load_question
+    @question = Question.find_by id: params[:id]
+    unless @question
+      flash.now[:danger] = t "flash.danger.question_not_found"
+    end
   end
 
   def load_questions
